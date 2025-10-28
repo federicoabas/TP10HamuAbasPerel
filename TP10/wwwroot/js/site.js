d5f7375896f430eed926375a7eb00b82a82371a5
UrielHamu <49518164@est.ort.edu.ar>
2025-10-27 21:47:15 -0300

    a

--- TP10/wwwroot/js/site.js
+++ TP10/wwwroot/js/site.js
@@ -1,12 +1,13 @@
 ﻿function ponerFuncionFinal() {
     const contenedor = document.getElementById("respuesta")
-    let rtaVerificada=document.getElementById("verificado").value
+    let esCorrecta=document.getElementById("verificado").value
     let rtaActual=document.getElementById("rta").value
     
     contenedor.innerHTML = `
-    if(${rtaVerificada})
+    if(${esCorrecta})
     {<h3>Respuesta correcta!</h3>}
     else{<h3>Respuesta incorrecta!
         La respuesta correcta era: ${rta}</h3>}`
 }
+
 
